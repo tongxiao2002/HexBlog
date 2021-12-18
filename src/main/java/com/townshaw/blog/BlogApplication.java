fca631574b700940937486b4cba0473eed9af6f2
TownShaw <xt20020109@mail.ustc.edu.cn>
2021-12-19 00:57:55 +0800

    change indentation from tab to space

--- src/main/java/com/townshaw/blog/BlogApplication.java
+++ src/main/java/com/townshaw/blog/BlogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages = "com.townshaw.blog.mapper")
 public class BlogApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 }
