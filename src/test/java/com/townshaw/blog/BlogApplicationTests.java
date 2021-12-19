d4cb471a7ad1a1c4936715b1ad8c624a9a755a89
TownShaw <xt20020109@mail.ustc.edu.cn>
2021-12-19 22:51:48 +0800

    ignore test files

--- src/test/java/com/townshaw/blog/BlogApplicationTests.java
+++ src/test/java/com/townshaw/blog/BlogApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.List;
+
 @SpringBootTest
 class BlogApplicationTests {
 
@@ -14,13 +17,21 @@ class BlogApplicationTests {
 
     @Test
     public void testInsert() {
-        ArticleDO test_article = new ArticleDO(null, "TownShaw", "testtest", "testtest_article");
-        articleMapper.insertWithoutDate(test_article);
+        ArticleDO test_article = new ArticleDO("TownShaw", "testtest", "testtest_article");
+        articleMapper.insert(test_article);
     }
 
     @Test
     public void testSelectById() {
         ArticleDO test_article = articleMapper.selectById("718459e2-567a-11ec-b8e9-d9c8f93e746a");
         System.out.println(test_article.toString());
+    }
+
+    @Test
+    public void testSelectAllArticle() {
+        List<ArticleDO> articleList = articleMapper.selectAllArticle();
+        for (ArticleDO article : articleList) {
+            System.out.println(article.toString() + "\n");
+        }
     }
 }
